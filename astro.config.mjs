43909841792fc122218a09430eb423baf56d5417
Charlie Thomson <charlie@thmsn.dev>
2024-09-04 18:37:15 -0400

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://thmsn.dev",
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
+});
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = "ツ - Charlie Thomson";
+export const SITE_DESCRIPTION = `Charlie Thomson's blog`;
